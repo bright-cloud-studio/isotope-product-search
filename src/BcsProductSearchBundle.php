d31b1bd3354db06f2056e69aaf961bd329f43084
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-02-18 13:23:26 -0500

    Create BcsProductSearchBundle.php

---
+++ src/BcsProductSearchBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Glide
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/isotope-product-search
+ */
+
+namespace Bcs\ProductSearchBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsProductSearchBundle extends Bundle
+{
+}
