d6b92ceee58b19cb2f58288ec68064608066f135
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-02-18 15:45:51 -0500

    Create tl_module.php

---
+++ src/Resources/contao/dca/tl_module.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Isotope List Ordered Products
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-list-ordered-products
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+// Palette for our module
+$GLOBALS['TL_DCA']['tl_module']['palettes']['list_prod_var'] = '{title_legend},name,headline,type;{config_legend},numberOfItems,perPage,iso_category_scope,iso_list_where,iso_newFilter,iso_filterModules,iso_listingSortField,iso_listingSortDirection;{redirect_legend},iso_link_primary,iso_jump_first,iso_addProductJumpTo,iso_wishlistJumpTo;{reference_legend:hide},defineRoot;{template_legend:hide},customTpl,iso_list_layout,iso_gallery,iso_cols,iso_use_quantity,iso_hide_list,iso_disable_options,iso_includeMessages,iso_emptyMessage,iso_emptyFilter,iso_buttons;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['figur_products'] = '{title_legend},name,headline,type;{redirect_legend},iso_link_primary,iso_addProductJumpTo;{template_legend:hide},customTpl,iso_list_layout,iso_use_quantity,iso_buttons;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
